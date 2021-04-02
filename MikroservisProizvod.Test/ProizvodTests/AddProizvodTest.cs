efbb0615b00fd35ff42da22b65722c46990d2e35
Tijana Milosevic <tijana.milosevic@saga.rs>
2021-04-02 21:11:02 +0200

    tests fix

--- MikroservisProizvod.Test/ProizvodTests/AddProizvodTest.cs
+++ MikroservisProizvod.Test/ProizvodTests/AddProizvodTest.cs
@@ -5,6 +5,7 @@ using FluentValidation;
 using FluentValidation.Results;
 using MikroServisProizvod.Application.ICommands;
 using MikroServisProizvod.Application.ICommands.Commands.Models;
+using MikroServisProizvod.Application.SeparatedModels;
 using MikroServisProizvod.Implementation.CommandImplementations.Proizvod.Commands;
 using Moq;
 using NUnit.Framework;
@@ -19,17 +20,17 @@ namespace MikroservisProizvod.Test.ProizvodTests
         private Mock<IValidator<ProizvodDto>> _mockValidator;       
         private IAddProzivodCommand _addProizvodCommand;
         private ProizvodDto proizvodToAdd;
-        private Proizvod mappedProizvod;
+        private ReadProizvodDto mappedProizvodForReturn;
+        private Proizvod proizvodFromDbo;
 
         [SetUp]
         public void Setup()
         {
             _mockGenericRepository = new Mock<IGenericRepository<Proizvod>>();
             _mockMapper = new Mock<IMapper>();
-            _mockValidator = new Mock<IValidator<ProizvodDto>>();
-            
+            _mockValidator = new Mock<IValidator<ProizvodDto>>();            
             _addProizvodCommand = new AddProizvodCommand(_mockGenericRepository.Object, _mockMapper.Object, _mockValidator.Object);
-
+            
             proizvodToAdd = new ProizvodDto
             {
                 Id = 0, // korisnik ne salje id pri dodavanju proizvoda
@@ -40,8 +41,71 @@ namespace MikroservisProizvod.Test.ProizvodTests
                 TipProizvodaId = 1,
                 Dobavljaci = new List<long> { 1 }
             };
+            mappedProizvodForReturn = new ReadProizvodDto
+            {
+                Id = 11,
+                Naziv = "Proizvod 1",
+                Cena = 11.1,
+                Pdv = 0.11,
+                JedinicaMere = new JedinicaMereDto
+                {
+                    Id = 1,
+                    Naziv = "Jedinica mere 1"
+                },
+                TipProizvoda = new TipProizvodaDto
+                {
+                    Id = 1,
+                    Naziv = "Tip proizvoda 1"
+                },
+                Dobavljaci = new List<DobavljacDto>
+                {
 
-            mappedProizvod = new Proizvod
+                    new DobavljacDto
+                    {
+                        Id = 1,
+                        Naziv = "Dobavljac 1"
+                    }
+                }
+            };
+            proizvodFromDbo = new Proizvod
+            {
+                Id = 11,
+                Naziv = "Proizvod 1",
+                Cena = 11.1,
+                Pdv = 0.11,
+                JedinicaMere = new JedinicaMere
+                {
+                    Id = 1,
+                    Naziv = "Jedinica mere 1"
+                },
+                TipProizvoda = new TipProizvoda
+                {
+                    Id = 1,
+                    Naziv = "Tip proizvoda 1"
+                },
+                Dobavljaci = new List<ProizvodDobavljac>
+                {
+                    new ProizvodDobavljac{
+                        Dobavljac = new Dobavljac
+                        {
+                            Id = 1,
+                            PIB = "123",
+                            Napomena = "Napomena",
+                            Naziv = "Dobavljac 1"
+                        }
+                    }
+                }
+            };
+
+        }
+
+        [Test]
+        public void AddProizvod()
+        {
+            // priprema
+            long newId = 11;
+
+           Proizvod mappedProizvod = new Proizvod
             {
                 Id = 0,
                 Naziv = "Proizvod 1",
@@ -70,12 +134,6 @@ namespace MikroservisProizvod.Test.ProizvodTests
                     }
                 }
             };
-        }
-
-        [Test]
-        public void AddProizvod()
-        {
-            // priprema
 
             _mockValidator.Setup(validator => validator.Validate(proizvodToAdd))
                 .Returns(new ValidationResult());
@@ -83,8 +141,14 @@ namespace MikroservisProizvod.Test.ProizvodTests
             _mockMapper.Setup(m => m.Map<Proizvod>(proizvodToAdd))
                 .Returns(mappedProizvod);
 
+            _mockMapper.Setup(m => m.Map<ReadProizvodDto>(proizvodFromDbo))
+                .Returns(mappedProizvodForReturn);
+
             _mockGenericRepository.Setup(gr => gr.Add(mappedProizvod))
-                .Callback(() => { mappedProizvod.Id = 11; });
+                .Callback(() => { mappedProizvod.Id = newId; });
+
+            _mockGenericRepository.Setup(gr => gr.FirstOrDefault(p => p.Id == mappedProizvod.Id, "JedinicaMere,TipProizvoda,Dobavljaci.Dobavljac"))
+                .Returns(proizvodFromDbo);
 
             // izvrsenje
             var result = _addProizvodCommand.Execute(proizvodToAdd);
@@ -92,7 +156,7 @@ namespace MikroservisProizvod.Test.ProizvodTests
             // provera
             _mockGenericRepository.Verify(gr => gr.Add(mappedProizvod), Times.Once());
             Assert.IsNotNull(result);
-            Assert.AreEqual(11, result.Id);
+            Assert.AreEqual(newId, result.Id);
 
         }
 
